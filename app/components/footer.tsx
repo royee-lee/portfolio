5253a11c86ab9df730bc5aeae7d2b7ed4ff42ed6
Roy Lee <rlee09@cs.students.ubc.ca>
2023-12-01 16:14:17 -0800

    update footer colours

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,13 +2,18 @@ import './../globals.css';
 
 export default function Footer() {
   return (
-    <footer className="fixed left-0 bottom-0 w-full p-8 pb-8 border-t border-purple bg-black font-inconsolata text-center">
+    <footer className="fixed left-0 bottom-0 w-full p-8 pb-8 border-t border-t-4 border-purple bg-black text-white font-inconsolata text-center">
       <p>
-        <a className="p-4 hover:underline" href="resume.pdf" target="_blank" rel="noopener">
+        <a
+          className="p-4 hover:underline decoration-purple"
+          href="resume.pdf"
+          target="_blank"
+          rel="noopener"
+        >
           [Résumé]
         </a>
         <a
-          className="p-4 hover:underline"
+          className="p-4 hover:underline decoration-purple"
           href="mailto:lee9uanlin@gmail.com"
           target="_blank"
           rel="noopener"
@@ -16,7 +21,7 @@ export default function Footer() {
           [Email]
         </a>
         <a
-          className="p-4 hover:underline"
+          className="p-4 hover:underline decoration-purple"
           href="https://github.com/9uanlin"
           target="_blank"
           rel="noopener"
@@ -24,7 +29,7 @@ export default function Footer() {
           [GitHub]
         </a>
         <a
-          className="p-4 hover:underline"
+          className="p-4 hover:underline decoration-purple"
           href="https://www.linkedin.com/in/9uanlin"
           target="_blank"
           rel="noopener"
