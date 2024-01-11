4bd482bc74c763e0408a2c41f2a3e90f6353a0d8
9uanlin <lee9uanlin@gmail.com>
2024-01-11 14:50:31 -0800

    Add some notes to main blog

--- app/page.tsx
+++ app/page.tsx
@@ -13,13 +13,13 @@ export default function Home() {
         <p className="font-inconsolata mt-4 leading-tight">
           I'm interested in{' '}
           <a className="font-bold underline decoration-accent">software engineering</a> and{' '}
-          <a className="font-bold underline decoration-accent">machine learning</a>, and am trying
-          to pick one of these areas to focus on. I currently study{' '}
+          <a className="font-bold underline decoration-accent">machine learning</a>, and sometimes I
+          like to fry my brain with algorithms. I currently study{' '}
           <a className="font-bold underline decoration-accent">Computer Science</a> @{' '}
           <a className="font-bold underline decoration-accent" href="https://www.ubc.ca/">
             The University of British Columbia
           </a>
-          , where I'm coming off of 1 year of co-op and entering my 4th year, w/ 1 more year to go.
+          , where I'm coming off of 1 year of co-op and entering my 4th year.
         </p>
         <p className="font-inconsolata mt-4 leading-tight">
           I'm also looking for an internship for the summer of 2024! Previously, I've worked on
@@ -53,7 +53,7 @@ export default function Home() {
           </a>
           . If you're interested in my cooking, take a look at my{' '}
           <a className="font-bold underline decoration-accent" href="">
-            menu
+            menu (WIP)
           </a>
           . If you want to see someone getting rowdy, click{' '}
           <a
@@ -65,7 +65,16 @@ export default function Home() {
           .
         </p>
 
-        <h2 className="font-roboto_slab text-xl underline decoration-accent mt-4">tldrs</h2>
+        <h2 className="font-roboto_slab text-xl underline decoration-accent mt-4">Notes</h2>
+        <p className="font-inconsolata mt-4 leading-tight">
+          This is still a WIP - I'll be adding bits here and there when I can get a breather from
+          homework. There's a lot I want to add and write about but my Advanced Algorithms and
+          Machine Learning and Data Mining classes are basically my life right now. Thanks for
+          checking my site out and look forward to some cool projects in the future!
+        </p>
+        <p className="font-inconsolata mt-4 leading-tight">
+          P.S. Sorry mobile viewers, I haven't fixed the CSS yet :{'('}
+        </p>
       </div>
     </div>
   );
